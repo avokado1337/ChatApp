a956b393892d4a724933f7dd7ba632aaa77657f8
Саразбанов Сұлтан Балабекұлы <sultan.sarazbanov@bas.kz>
2022-02-24 19:24:46 +0600

    initial commit

---
+++ Data/ChatDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Chat.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Chat.Data
+{
+    public class ChatDbContext : IdentityDbContext
+    {
+        public ChatDbContext(DbContextOptions<ChatDbContext> options) : base(options) { }
+
+        public DbSet<Message> Messages { get; set; }
+
+    }
+}
---
+++ Models/Message.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Chat.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+    }
+}
